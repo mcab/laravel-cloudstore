ca4d2692d9c1452ad5a0ad43e8728f390ad1660e
Mark Cabanero <me@markcabanero.com>
2017-05-12 12:57:49 -0400

    Basic logic to save file information to database + upload.

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\File;
 use Illuminate\Http\Request;
 
 class FilesController extends Controller
@@ -12,6 +13,19 @@ class FilesController extends Controller
     }
     public function store()
     {
+        if (! $name = request('name')) {
+            $name = request('file')->getClientOriginalName();
+        }
+        if (! $description = request('description')) {
+            $description = "";
+        }
+
+        $file = new File;
+        $file->name = $name;
+        $file->description = $description;
+        $file->user_id = auth()->user()->id;
+        $file->save();
+
         request()
             ->file('file')
             ->store('public/' . auth()->user()->unique_id);
