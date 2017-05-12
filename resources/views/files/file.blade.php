79d0d499d2b0714a8f5d6ade696bfa28ef9d43e0
Mark Cabanero <me@markcabanero.com>
2017-05-12 15:26:05 -0400

    Create a URL that pushes to the path of the files uploaded + unique user hash + file hash.

--- resources/views/files/file.blade.php
+++ resources/views/files/file.blade.php
@@ -2,5 +2,5 @@
     <td><span class="text-overflow">{{ $file->name }}</span></td>
     <td>{{ $file->description }}</td>
     <td>{{ $file->created_at->toFormattedDateString() }}</td>
-    <td class="text-right"><a href="{{ Storage::disk('local')->url(auth()->user()->unique_id . '/' . $file->file_id) }}"><span class="glyphicon glyphicon-save" aria-hidden="true"></span></a></td>
+    <td class="text-right"><a href="{{ Storage::url(auth()->user()->unique_id . '/' . $file->file_id) }}"><span class="glyphicon glyphicon-save" aria-hidden="true"></span></a></td>
 </tr>
