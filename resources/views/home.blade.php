9adb07205e634d25b6e1ba622e047a85edcc2197
Mark Cabanero <me@markcabanero.com>
2017-05-12 11:10:49 -0400

    Basic styling of upload form.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,27 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Upload a File</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                     <form method="POST" action="/files">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name"></input>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea class="form-control" id="description" name="description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="file" name="file"></input>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Save File</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
