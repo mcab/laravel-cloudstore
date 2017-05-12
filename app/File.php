298e475900b9f56a5a16d7f3bcbe8247b95bdd90
Mark Cabanero <me@markcabanero.com>
2017-05-12 11:04:37 -0400

    Moved index() method to controller.

--- app/File.php
+++ app/File.php
@@ -4,8 +4,5 @@ namespace App;
 
 class File extends Model
 {
-    public function index()
-    {
 
-    }
 }
--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class FilesController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
 }
